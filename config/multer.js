1bf117ef7cf4727c0d6b38044862fa04949602b8
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2022-01-18 21:26:06 +0100

    multer config

---
+++ config/multer.js
@@ -0,0 +1,22 @@
+const multer  = require('multer');
+const helpers = require('./helper');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images')
+    },
+    filename: (req, file, cb) => {
+        const extension = file.originalname.split('.');
+        cb(null, Date.now()+'.'+extension[extension.length - 1])
+    }
+});
+
+
+const singleUpload = multer({
+    storage: storage, 
+    // limits: {fileSize: 1024 * 1024 },
+    fileFilter: helpers.imageFilter
+}).single('picture');
+
+module.exports = {
+    singleUpload
+}
